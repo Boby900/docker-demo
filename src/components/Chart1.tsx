c3c22bda27db0b9d259a13e3cde7043ddd6791c3
Boby900 <bobytiwari310@gmail.com>
2024-08-26 20:38:40 +0530

    setting up charts

---
+++ src/components/Chart1.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Chart1 = () => {
+  return (
+    <div>Chart1</div>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <h1 className="text-white">Dashboard</h1>
+    </div>
+  )
+}
+
