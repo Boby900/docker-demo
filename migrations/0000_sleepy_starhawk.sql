bf32347e93b28eb1e993cf237b7fc4445f551ad6
Boby900 <bobytiwari310@gmail.com>
2024-09-01 19:36:38 +0530

    done setting up pg with docker

---
+++ migrations/0000_sleepy_starhawk.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" varchar(50) NOT NULL,
+	"email" varchar(100) NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
